819cc76e4a68f47c27c86f7ac2ca1b91fdb762bd
Colin Travis <colint@cybertravis.us>
2019-01-26 21:33:11 -0600

    Camera drag

---
+++ Assets/Scripts/Camera/CameraController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+
+    public GameObject followTarget;
+
+    private Vector3 targetPos;
+    public float cameraDrag;
+
+//    public Vector3 offset;
+    public float zOffset;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        targetPos = new Vector3(followTarget.transform.position.x, transform.position.y, followTarget.transform.position.z + zOffset);
+        transform.position = Vector3.Lerp( transform.position, targetPos, cameraDrag * Time.deltaTime );
+    }
+}
+
+
+
+
+
--- Assets/Scripts/DetectControlMethod.cs
+++ Assets/Scripts/DetectControlMethod.cs
@@ -16,15 +16,6 @@ public class DetectControlMethod : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-
-        if (thePlayer.useController == true)
-        {
-            print("Input set to controller.");
-        }
-        else
-        {
-            print("Input set to mouse.");
-        }
         
         //Detect Mouse Input
         if (Input.GetMouseButton(0) || Input.GetMouseButton(1) || Input.GetMouseButton(2))
@@ -43,17 +34,17 @@ public class DetectControlMethod : MonoBehaviour
             thePlayer.useController = true;
         }
 
-        if (Input.GetKey(KeyCode.JoystickButton0) ||
-            Input.GetKey(KeyCode.JoystickButton1) ||
-            Input.GetKey(KeyCode.JoystickButton2) ||
-            Input.GetKey(KeyCode.JoystickButton3) ||
-            Input.GetKey(KeyCode.JoystickButton4) ||
-            Input.GetKey(KeyCode.JoystickButton5) ||
-            Input.GetKey(KeyCode.JoystickButton6) ||
-            Input.GetKey(KeyCode.JoystickButton7) ||
-            Input.GetKey(KeyCode.JoystickButton8) ||
-            Input.GetKey(KeyCode.JoystickButton9) ||
-            Input.GetKey(KeyCode.JoystickButton10))
+        if (Input.GetKey(KeyCode.Joystick1Button0) ||
+            Input.GetKey(KeyCode.Joystick1Button1) ||
+            Input.GetKey(KeyCode.Joystick1Button2) ||
+            Input.GetKey(KeyCode.Joystick1Button3) ||
+            Input.GetKey(KeyCode.Joystick1Button4) ||
+            Input.GetKey(KeyCode.Joystick1Button5) ||
+            Input.GetKey(KeyCode.Joystick1Button6) ||
+            Input.GetKey(KeyCode.Joystick1Button7) ||
+            Input.GetKey(KeyCode.Joystick1Button8) ||
+            Input.GetKey(KeyCode.Joystick1Button9) ||
+            Input.GetKey(KeyCode.Joystick1Button10))
         {
             thePlayer.useController = true;
         }
